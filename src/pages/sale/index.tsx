eddbf8b9ea528c56b592a3837cb9a787da3451ce
LAPTOP-KNPVO1TB\86136 <caiping@ypshengxian.com>
2020-12-14 17:16:48 +0800

    .

--- src/pages/sale/index.tsx
+++ src/pages/sale/index.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
-import { View } from '@tarojs/components'
+import './index.styl'
+import { View, ScrollView, Text, Image } from '@tarojs/components'
 
 const List = () => {
-  
-  return  <View>实时销售</View>
+
+  return (
+    <View className={'content'}>
+      <View className="top">
+        <Text className="left">今日数据</Text>
+        <View className="right">2020-12-01 16:09<View>??</View></View>
+      </View>
+      <View className={'cardbox'}>
+        <View className="item">
+          <Text className={'title'}>今日营业额</Text>
+          <View className="numbox"><Text className="icon">¥</Text><Text className={'num'}>198.00</Text></View>
+        </View>
+        <View className="item">
+          <Text className={'title'}>已售份数</Text>
+          <View className="numbox"><Text className="num">198</Text></View>
+        </View>
+      </View>
+      <View className={'list'}>
+        <View className="list-item">
+          <Image className='img'
+            src='https://camo.githubusercontent.com/3e1b76e514b895760055987f164ce6c95935a3aa/687474703a2f2f73746f726167652e333630627579696d672e636f6d2f6d74642f686f6d652f6c6f676f2d3278313531333833373932363730372e706e67'
+          />
+          <View className="right">
+            <View className="head">汉釜宫彩虹芝士夹心年糕（南瓜紫薯鹭明雪花培根  500g/袋</View>
+            <View className="bottom">
+              <Text>已售<Text className="strong">20</Text>份</Text>
+              <Text>剩余：<Text className="strong blue-font">20</Text></Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
 export default List
