1bfeae4fa498013e2b2ce08c8d0acbfb47d4cc4a
Dora <chenweiqing@ypshengxian.com>
2020-12-16 12:04:34 +0800

    update

--- src/pages/withdraw/index.tsx
+++ src/pages/withdraw/index.tsx
@@ -3,6 +3,7 @@ import { View } from '@tarojs/components'
 import styles from './index.module.styl'
 import bgImg from '@/images/bg.png'
 import Header from '@/components/header'
+import { AtIcon, AtToast } from 'taro-ui'
 
 const Withdraw = () => {
   
@@ -13,9 +14,10 @@ const Withdraw = () => {
         <View className='flex_center_center_column'>
           <View className={styles.can_text}>可提现金额</View>
           <View className={styles.amount_txt}>¥18,540.00</View>
-          <View className={styles.froze_txt}>已冻结¥100.00</View>
+          <View className={`${styles.froze_txt} flex_center_center_row`}>已冻结¥100.00</View>
         </View>
       </View>
+      <AtToast isOpened text='由于密码错误次数已达到上限导致资金冻结，请稍后尝试提现' duration={5000} />
     </View>
   )
 }
