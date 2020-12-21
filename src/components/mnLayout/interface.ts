e10a9e62a53d3146b8b05b6640ce36c86a3b12f7
lichengtao <372425159@qq.com>
2020-12-21 17:17:48 +0800

    feat:添加layout route工具方法2

--- src/components/mnLayout/interface.ts
+++ src/components/mnLayout/interface.ts
@@ -1,10 +1,11 @@
 export interface IPb {
   title: string //  显示的title
-  path: string // 传入跳转的路径，如果不传就是默认返回上一级
+  path?: string // 传入跳转的路径，如果不传就是默认返回上一级
   backTitle?: string // 返回的显示文案
   statusBarStyle?: object // 自定义StatusBar 的样式
   hideStatusBar?: boolean // 隐藏 顶部的StatusBar
   gotoType?: number // 跳转类型
+  hideArrow?: boolean //
 }
 export interface IPt {
   dataList: object[]
--- src/components/mnLayout/nav.tsx
+++ src/components/mnLayout/nav.tsx
@@ -8,10 +8,10 @@ import './nav.less'
 import RouteImg from './route1.png'
 
 const MnNav = (IProps: IPb) => {
-
+    const { hideArrow, path } = IProps
     const jumpFunc = () => {
-        const { path } = IProps
-        console.log('path', path)
+
+
         if (path) {
             goTo(path, IProps.gotoType || 3) // reLaunch 关闭所有页面，打开到应用内的某个页面
         } else {
@@ -20,11 +20,12 @@ const MnNav = (IProps: IPb) => {
     }
     return (
         <View className='mnNav_box'>
+            {hideArrow && !path ? <View className='leftPart' >
+            </View> : <View className='leftPart' onClick={jumpFunc}>
+                    <Image className='routeImg' src={RouteImg}></Image>
+                    <Text className='backTitle fos'>{IProps.backTitle || ''}</Text>
+                </View>}
 
-            <View className='leftPart' onClick={jumpFunc}>
-                <Image className='routeImg' src={RouteImg}></Image>
-                <Text className='backTitle fos'>{IProps.backTitle || ''}</Text>
-            </View>
             <View className='midPart'>
                 <Text className='title fos'>{IProps.title || ''}</Text>
             </View>
