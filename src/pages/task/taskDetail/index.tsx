4de10b80273954784e35a44c6984fad13443650e
LAPTOP-KNPVO1TB\86136 <caiping@ypshengxian.com>
2020-12-22 16:07:12 +0800

    .

--- src/pages/task/taskDetail/index.tsx
+++ src/pages/task/taskDetail/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { View, Text, Image } from '@tarojs/components'
 // import { Picker } from '@tarojs/components'
 import List, { ListLayout } from '@/components/list'
+import MnLayout from '@/components/mnLayout'
 import styles from './index.module.styl'
 const Taskd = () => {
   const [goodList, setGoodList] = useState([])
@@ -36,9 +37,9 @@ const Taskd = () => {
   const listItem = (it) => {
     return (
       <View className={styles.list_item}>
-        <Image className={styles.img}
+        {/* <Image className={styles.img}
           src='https://camo.githubusercontent.com/3e1b76e514b895760055987f164ce6c95935a3aa/687474703a2f2f73746f726167652e333630627579696d672e636f6d2f6d74642f686f6d652f6c6f676f2d3278313531333833373932363730372e706e67'
-        />
+        /> */}
         <View className={styles.right}>
           <View className={styles.head}>汉釜宫彩虹芝士夹心年糕（南瓜紫薯鹭明雪花培根  500g/袋</View>
           <View className={styles.bottom}>
@@ -49,19 +50,21 @@ const Taskd = () => {
     )
   }
   return (
-    <ListLayout className={styles.content}>
-      <View className={styles.mes}>
-        <View className={`at-icon at-icon-volume-minus ${styles.vol}`}></View>
-        <Text>每日晚22:00更新截单</Text>
-      </View>
-      <View className={styles.top_time}>
-        <Text>截单时间：2020-11-28 22:00:00</Text>
-        <Text>合计：226份</Text>
-      </View>
-      <List onScrollToLower={onScrollToLower} showLoadMore={loading}>
-        {goodList.map(item => listItem(item))}
-      </List>
-    </ListLayout>
+    <MnLayout title='生产清单' hideArrow={false}>
+      <ListLayout className={styles.content}>
+        <View className={styles.mes}>
+          <View className={`at-icon at-icon-volume-minus ${styles.vol}`}></View>
+          <Text>每日晚22:00更新截单</Text>
+        </View>
+        <View className={styles.top_time}>
+          <Text>截单时间：2020-11-28 22:00:00</Text>
+          <Text>合计：226份</Text>
+        </View>
+        <List onScrollToLower={onScrollToLower} showLoadMore={loading}>
+          {goodList.map(item => listItem(item))}
+        </List>
+      </ListLayout>
+    </MnLayout>
   )
 }
 export default Taskd
